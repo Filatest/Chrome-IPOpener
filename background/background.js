7141d192f4f1226c78e1aab2a1d7a7e17d55ece8
Filatest <sounds_gnocchi.0w@icloud.com>
2024-08-22 12:32:23 +0800

    增加输入框聚焦

--- background/background.js
+++ background/background.js
@@ -1,16 +1,16 @@
-// 创建一个只在浏览器图标上显示的上下文菜单项
-chrome.contextMenus.create({
-  id: 'my-extension-config',
-  title: '配置插件',
-  contexts: ['action']  // 更改为 'browser_action'
-});
+// // 创建一个只在浏览器图标上显示的上下文菜单项
+// chrome.contextMenus.create({
+//   id: 'my-extension-config',
+//   title: '配置插件',
+//   contexts: ['action']  // 更改为 'browser_action'
+// });
 
-// 当点击菜单项时打开选项页面
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  if (info.menuItemId === 'my-extension-config') {
-    chrome.tabs.create({ url: chrome.runtime.getURL('options/options.html') });
-  }
-});
+// // 当点击菜单项时打开选项页面
+// chrome.contextMenus.onClicked.addListener(function(info, tab) {
+//   if (info.menuItemId === 'my-extension-config') {
+//     chrome.tabs.create({ url: chrome.runtime.getURL('options/options.html') });
+//   }
+// });
 
 // background.js
 chrome.runtime.onInstalled.addListener(function(details) {
